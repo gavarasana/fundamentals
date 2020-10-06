ef28357ca04c6e5fe4b85aade589c7829724862e
Ravi Gavarasana <gavarasana@gmail.com>
2020-10-05 20:11:53 -0400

    added h2 database

--- src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
+++ src/main/java/com/pluralsight/fundamentals/FundamentalsApplication.java
@@ -28,13 +28,36 @@ public class FundamentalsApplication {
 		return (args) -> {
 			Application application = new Application("Trackzilla", "A bug tracking application", "Kesha Williams");
 			appRepository.save(application);
-			appRepository.save(new Application("Expenses", "An application to submit expenses", "Jane Doe"));
-			appRepository.save(new Application("Bookings", "An application used to book tickets", "JMary Richards"));
+			Application application2 = new Application("Expenses", "An application to submit expenses", "Jane Doe");
+			appRepository.save(application2);
 
-			Release release = new Release("2020-09-12", "desc1");
+			Application application3 = new Application("Bookings", "An application used to book tickets", "John Doe");
+			appRepository.save(application3);
+
+			Application application4 = new Application("Invoice Search", "An application used to search invoices", "Mary Richards");
+			appRepository.save(application4);
+
+			Application application5 = new Application("Audits", "An application used for auditing purposes", "Tiffany Stewart");
+			appRepository.save(application5);
+
+
+			Release release = new Release("2020-03-32", "Q1 Release containing high priority bugs");
 			releaseRepository.save(release);
+			ticketRepository.save(new Ticket("Sort Feature", "Add the ability to sort tickers by severity", "OPEN",application2, release ));
 
-			ticketRepository.save(new Ticket("test1", "desc1", "status1",application, release ));
+			release = new Release("2020-06-29", "Q2 Release containing high priority enhancements");
+			releaseRepository.save(release);
+			ticketRepository.save(new Ticket("Search Feature", "Add the ability to search by invoice date", "IN PROGRESS",application4, release ));
+
+			release = new Release("2020-09-21", "Q3 Release containing bugs");
+			releaseRepository.save(release);
+			ticketRepository.save(new Ticket("Audit", "Add the ability to audit by year", "CLOSED",application5, release ));
+
+			release = new Release("2020-12-05", "Q4 Release containing enhancements");
+			releaseRepository.save(release);
+			ticketRepository.save(new Ticket("Booking Feature", "Add the ability to book tickets online", "OPEN",application3, release ));
+
+
 
 		};
 	}
